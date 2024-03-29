7f9ff0c5891e1526b7674ded701026a50c032448
Akisa-Moruri <akisamoruri@gmail.com>
2024-03-29 10:51:35 +0300

    array map method index.js

--- index.js
+++ index.js
@@ -11,6 +11,15 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+function titleCased() {
+  function titleCaseWord(word) {
+    // Capitalize the first letter of the word and add the rest of the word
+    return word.charAt(0).toUpperCase() + word.slice(1);
+  }
+
+  // Split each string into an array of words, apply the `titleCaseWord()` function to each word, and then join the words back together
+  return tutorials.map(title => title.split(' ').map(titleCaseWord).join(' '));
 }
+
+// Example usage
+console.log(titleCased());
